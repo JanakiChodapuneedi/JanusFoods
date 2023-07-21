fe9d857bf7c9b994712c2dc001499443f579c70c
JanakiChodapuneedi <janakichodapuneedi@gmail.com>
2023-07-21 15:13:31 +0530

    added parcel

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const heading= React.createElement("h1",{id:"heading"},"hello from react");
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
