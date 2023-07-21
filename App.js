2d0ce4c44a7904f365ef3b997fd949d953e32df7
JanakiChodapuneedi <janakichodapuneedi@gmail.com>
2023-07-21 19:59:59 +0530

    components

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const heading= React.createElement("h1",{id:"heading"},"hello from react");
-console.log(heading);
+const HeadingComponent = () => (<h1>
+    Hello from react using jsx 
+    </h1>);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
