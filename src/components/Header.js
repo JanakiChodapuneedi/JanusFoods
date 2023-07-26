d19f92e4b4e5fd1b093d8cc968109c81649134bb
JanakiChodapuneedi <janakichodapuneedi@gmail.com>
2023-07-26 15:51:46 +0530

    added top rated rest  filter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
+import {LOGO_URL} from "./../utils/constants";
 const Header = () => {
     return (
       <div className="header">
         <img
           className="logo"
-          src="https://images-platform.99static.com//64RVHLYKS-sAgw4WkNz2PauaEPQ=/117x65:1122x1070/fit-in/590x590/99designs-contests-attachments/121/121740/attachment_121740866"
+          src= {LOGO_URL}
         />
         <ul>
           <li> Home </li>
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+ export const RES_CDN_URL = " bvcb";
+ export const  LOGO_URL="https://images-platform.99static.com//64RVHLYKS-sAgw4WkNz2PauaEPQ=/117x65:1122x1070/fit-in/590x590/99designs-contests-attachments/121/121740/attachment_121740866";
