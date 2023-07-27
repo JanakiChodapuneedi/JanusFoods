8981ffdb6bfdc164f2d03869a0b3a8bf93fb04fd
JanakiChodapuneedi <janakichodapuneedi@gmail.com>
2023-07-27 15:38:19 +0530

    search implemeted

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,24 +4,47 @@ import { useState,useEffect } from "react";
 import {SWIGGY_API} from "./../utils/constants";
 import Shimmer from "./Shimmer";
 const Body = () => {
+  const [allRest,setAllRest] = useState([]);
   const [filteredRest, setFilteredRest] = useState([]);
+  const[searchTxt,setSearchTxt]=useState("");
   function getTopRatedRest() {
-    const filterRest = restList.filter((rest) => rest.info.avgRating > 4);
+    const filterRest = filteredRest.filter((rest) => rest.info.avgRating > 4);
     setFilteredRest(filterRest);
   }
   const fetchData= async() =>{
     const data = await fetch(SWIGGY_API);
     const json = await data.json();
     console.log(json);
-    setFilteredRest(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants); 
+    //to get restaurants in caurosel
+    setFilteredRest(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants); 
+    //to get restaurants in grid
+   // setFilteredRest(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants); 
+    setAllRest(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }
+  function  searchFilteredRest(){
+    const filtRest = allRest.filter((rest) => rest.info.name.toUpperCase().includes(searchTxt.toUpperCase()));
+    setFilteredRest(filtRest);
+    console.log("searchFilteredRest called");
+   
   }
   useEffect(()=>{
     fetchData();
   },[]);
   return (
-    filteredRest.length!=0 ?
+    filteredRest.length === 0 ? <Shimmer/>:
     <div>
       <div className="filter">
+        <div className="search">
+          <input type="text" 
+          className="search-box"
+           value={searchTxt}
+           onChange={(e)=>{
+            setSearchTxt(e.target.value);
+      
+           }}/>
+          <button className="search-btn" onClick={()=>{
+            searchFilteredRest();}}>Search</button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -36,8 +59,7 @@ const Body = () => {
           <RestaurantCard key={rest.info.id} resData={rest} />
         ))}
       </div>
-    </div> :
-    <Shimmer/>
+    </div> 
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,29 @@
-import {LOGO_URL} from "./../utils/constants";
+import { useState } from "react";
+import { LOGO_URL } from "./../utils/constants";
 const Header = () => {
-    return (
-      <div className="header">
-        <img
-          className="logo"
-          src= {LOGO_URL}
-        />
-        <ul>
-          <li> Home </li>
-          <li> About US</li>
-          <li> Contact US </li>
-          <li> Cart </li>
-        </ul>
-      </div>
-    );
-  };
+  const [btnName, setBtnName] = useState("Login");
+  return (
+    <div className="header">
+      <img className="logo" src={LOGO_URL} />
+      <ul>
+        <li> Home </li>
+        <li> About US</li>
+        <li> Contact US </li>
+        <li> Cart </li>
+        <button className="login-btn"
+          onClick={() => {
+            if (btnName === "Login") {
+              setBtnName("Logout");
+            } else {
+              setBtnName("Login");
+            }
+          }}
+        >
+          {btnName}
+        </button>
+      </ul>
+    </div>
+  );
+};
 
-  export default Header;
+export default Header;
